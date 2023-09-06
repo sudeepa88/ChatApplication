fe5e2d3a309e22270963937367a22614fe47df99
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-09-07 00:09:57 +0530

    Added all

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import redis
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,7 +97,9 @@ CHANNEL_LAYERS = {
         },
     },
 }
-
+r = redis.from_url(os.environ['redis://red-cjsc63lm702s73ftatt0:6379'])
+r.set('key', 'redis-py')
+r.get('key')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases

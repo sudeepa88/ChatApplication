52c6831f1c49943acfccd7c9a111345b8fc042ce
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-09-07 00:00:56 +0530

    Status updated

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -91,7 +91,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": ["redis://red-cjsc63lm702s73ftatt0:6379"],
         },
     },
 }

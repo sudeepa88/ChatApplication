68a46c27fdef04a138ce15b04de08a484a5aafe5
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-09-06 15:37:22 +0530

    Added all

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -78,11 +78,23 @@ TEMPLATES = [
 #WSGI_APPLICATION = 'djangochat.wsgi.application'
 ASGI_APPLICATION = 'djangochat.asgi.application'
 
-CHANNEL_LAYERS={
-    'default':{
-        'BACKEND':'channels.layers.InMemoryChannelLayer'
-    }
+# CHANNEL_LAYERS={
+#     'default':{
+#         'BACKEND':'channels.layers.InMemoryChannelLayer'
+#     }
+# }
+
+# settings.py
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -129,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
